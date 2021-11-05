5a8b645995a897e858063f36caa5f541dee11b5b
Dan802 <jfelipeg05@gmail.com>
2021-11-04 20:28:27 -0500

    Prototipo

--- Magia.js
+++ Magia.js
@@ -1,15 +1,38 @@
-const content = element.innerHTML;
+const main = document.querySelector('#principal');
 
-element.innerHTML = htmlString;
+//Ciclo creado de forma artesanal porque con un for por mas intentos que hice no me dio :')
+let variable = 0;
 
-document.body.innerHTML = "";
+function Esperar() {
 
-var counter = 1;
+  setInterval(function(){
 
-setInterval(function(){
-  document.getElementById('texto1' + counter).checked = true;
-  counter++;
-  if(counter > 4){
-    counter = 1;
-  }
-}, 5000);
+    if (variable === 0){
+      variable = 1;
+      console.log("woa morir")
+      console.log("woa morirx1")
+      console.log("woa morirx2")
+      main.innerHTML = `<p>Holaaaa</p>`;
+    }
+    else if (variable===1)
+    {
+      variable = 2;
+      console.log("Enserio woa morir")
+      console.log("Enserio woa morirx1")
+      console.log("Enserio woa morirx2")
+      main.innerHTML = `<p>como estas</p>`;
+    }
+    else if (variable===2)
+    {
+      variable = 0;
+      console.log("Mas enserio a no poder, woa morir")
+      console.log("Mas enserio a no poder, woa morirx1")
+      console.log("Mas enserio a no poder, woa morirx2")
+      main.innerHTML = `<p>bien y tu</p>`;
+    }
+  }, 6000);
+}
+
+Esperar();
+
+    about:blank
