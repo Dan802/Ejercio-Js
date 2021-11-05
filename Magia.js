d10fb44c45fa7df10057752a12ae49579bb573d2
Dan802 <jfelipeg05@gmail.com>
2021-11-04 21:46:22 -0500

    Finalizado UwU

--- Magia.js
+++ Magia.js
@@ -1,7 +1,17 @@
+//Aquí es donde ocurre la magia ^~^
 const main = document.querySelector('#principal');
+main.innerHTML = `<h1 class="title">La emoción es parte de vivir</h1>
+      <div class="lugar_Frase">
+      <p id="Frase" class="sentence">"Sin embargo, aunque no podemos adivinar el mundo que será, bien podemos
+          imaginar el que queremos que sea. El derecho de soñar no figura entre los
+          treinta derechos humanos que las Naciones Unidas proclamaron a fines de
+          1948. Pero si no fuera por él, y por las aguas que da de beber, los demás
+          derechos se morirían de sed"
+      </p>
+      <p id ="Autor" class="author">- Eduardo Galeano</p>
+      </div>`;
 
-//Ciclo creado de forma artesanal porque con un for por mas intentos que hice no me dio :')
-let variable = 0;
+let variable = 1;
 
 function Esperar() {
 
@@ -9,30 +19,61 @@ function Esperar() {
 
     if (variable === 0){
       variable = 1;
-      console.log("woa morir")
-      console.log("woa morirx1")
-      console.log("woa morirx2")
-      main.innerHTML = `<p>Holaaaa</p>`;
+      main.innerHTML = `<h1 class="title">La emoción es parte de vivir</h1>
+      <div class="lugar_Frase">
+      <p id="Frase" class="sentence">"Sin embargo, aunque no podemos adivinar el mundo que será, bien podemos
+          imaginar el que queremos que sea. El derecho de soñar no figura entre los
+          treinta derechos humanos que las Naciones Unidas proclamaron a fines de
+          1948. Pero si no fuera por él, y por las aguas que da de beber, los demás
+          derechos se morirían de sed"
+      </p>
+      <p id ="Autor" class="author">- Eduardo Galeano</p>
+      </div>`;
     }
     else if (variable===1)
     {
       variable = 2;
-      console.log("Enserio woa morir")
-      console.log("Enserio woa morirx1")
-      console.log("Enserio woa morirx2")
-      main.innerHTML = `<p>como estas</p>`;
+      main.innerHTML = `<h1 class="title">La emoción es parte de vivir</h1>
+      <div class="lugar_Frase">
+      <p id="Frase" class="sentence">"La educación es algo admirable, pero es bueno recordar
+          de cuando en cuando que nada de lo que merece la pena saber puede ser enseñado"
+      </p>
+      <p id ="Autor" class="author">-   Oscar Wilde</p>
+      </div>`;
     }
     else if (variable===2)
     {
+      variable = 3;
+      main.innerHTML = `<h1 class="title">La emoción es parte de vivir</h1>
+      <div class="lugar_Frase">
+      <p id="Frase" class="sentence">"Cuando es verdadera, cuando nace de la necesidad
+          de decir, a la voz humana no hay quien la pare. Si le niegan la boca, ella habla
+           por las manos, o por los ojos, opor los poros, o por donde sea"
+      </p>
+      <p id ="Autor" class="author">-   Eduardo Galeano</p>
+      </div>`;
+    }
+    else if (variable===3)
+    {
+      variable = 4;
+      main.innerHTML = `<h1 class="title">La emoción es parte de vivir</h1>
+      <div class="lugar_Frase">
+      <p id="Frase" class="sentence">"Nunca pensé que en la felicidad hubiera tanta tristeza"
+      </p>
+      <p id ="Autor" class="author">-   Mario Benedetti</p>
+      </div>`;
+    }
+    else if (variable===4)
+    {
       variable = 0;
-      console.log("Mas enserio a no poder, woa morir")
-      console.log("Mas enserio a no poder, woa morirx1")
-      console.log("Mas enserio a no poder, woa morirx2")
-      main.innerHTML = `<p>bien y tu</p>`;
+      main.innerHTML = `<h1 class="title">La emoción es parte de vivir</h1>
+      <div class="lugar_Frase">
+      <p id="Frase" class="sentence">"Las  cosas  simples  son  las  más  extraordinarias  y  sólo  los  sabios  consiguen verlas"
+      </p>
+      <p id ="Autor" class="author">-   Paulo Coelho</p>
+      </div>`;
     }
   }, 6000);
 }
 
 Esperar();
-
-    about:blank
